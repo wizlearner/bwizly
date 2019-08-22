69aefe064c9a395e821bc6ae6e91c8f6a0804c62
Khushboo khullar <khushbookhullar@Khushboos-MacBook-Air.local>
2019-08-22 19:34:32 +0800

    first commit

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,14 +1,25 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
-// The configuration below is not sensitive data. You can serenely add your config here
-const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
-}
 
-firebase.initializeApp(config)
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/6.4.0/firebase-app.js"></script>
+
+/// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#config-web-app -->
+
+// <script>
+  // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDF2oEsAFJHUVzOmHnlZIZQXgmNZkpLcU4",
+    authDomain: "bwizly.firebaseapp.com",
+    databaseURL: "https://bwizly.firebaseio.com",
+    projectId: "bwizly",
+    storageBucket: "",
+    messagingSenderId: "287168854865",
+    appId: "1:287168854865:web:aa867cf4cf450601"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+// </script>
